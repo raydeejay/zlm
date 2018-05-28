c72fb824523129c3053772d5f549af074a034810
Sergi Reyner <sergi.reyner@gmail.com>
2018-05-29 00:18:49 +0100

    Added SIGNED to turn an encoded 16-bit number into a lisp number

--- utils.lisp
+++ utils.lisp
@@ -25,3 +25,15 @@
                   (1- (ash 1 width)))
           (logand (ash x (- width (mod bits width)))
                   (1- (ash 1 width)))))
+
+(defun signed (n)
+  "In the Z-machine, numbers are usually stored in 2 bytes (in the
+form most-significant-byte first, then least-significant) and hold
+any value in the range $0000 to $ffff (0 to 65535 decimal).
+
+These values are sometimes regarded as signed, in the range -32768 to
+32767. In effect -n is stored as 65536-n and so the top bit is the
+sign bit."
+  (if (logbitp 15 n)
+      (- (- 65536 n))
+      n))
